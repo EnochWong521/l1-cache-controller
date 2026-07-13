b4e87f208b44c671c1d203331a8ac41e883cbed0
EnochWong521 <enoch3d14@gmail.com>
2026-07-12 23:52:36 -0700

    Cache lookup RTL code complete, to be tested

--- l1-cache-controller.srcs/sources_1/new/cache_lookup.sv
+++ l1-cache-controller.srcs/sources_1/new/cache_lookup.sv
@@ -7,42 +7,56 @@ module cache_lookup #(
 )(
     input logic clk,
     input logic rst_n,
-    input logic [$clog2(LINE_BYTES) - 1:0] offset,
     input logic [$clog2(NUM_LINES) - 1: 0] index,
     input logic [ADDR_W - $clog2(NUM_LINES) - $clog2(LINE_BYTES) - 1:0] tag,
+    
+    input logic fill_en,
+    input logic [$clog2(NUM_LINES) - 1: 0] fill_index, 
+    input logic [ADDR_W - $clog2(NUM_LINES) - $clog2(LINE_BYTES) - 1:0] fill_tag,
+    input logic [$clog2(LINE_BYTES * 8) - 1:0] fill_data,
     
     output logic [$clog2(LINE_BYTES * 8) - 1:0] data,
     output logic dirty,
     output logic valid,
     output logic hit
 );
-    int i;
-    // tag array
     localparam TAG_W = ADDR_W - $clog2(NUM_LINES) - $clog2(LINE_BYTES);
-    logic [TAG_W - 1:0] tag_arr [NUM_LINES];
+    localparam DATA_W = $clog2(LINE_BYTES * 8);
     
-    // data array
-    localparam DATA_W = LINE_BYTES * 8;
-    logic [DATA_W - 1:0] data_arr [NUM_LINES];
+    typedef struct {
+        logic [TAG_W - 1:0] tag;
+        logic [DATA_W - 1:0] data;
+        logic valid;
+        logic dirty;
+    } cache_line_t;
+    
+    cache_line_t cache_arr [NUM_LINES];
     
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            // zero out array
-            for (i = 0; i < NUM_LINES; i++) begin
-                tag_arr[i] <= '0;
-                data_arr[i] <= '0;
+            foreach (cache_arr[i]) begin
+                cache_arr[i].tag = '0;
+                cache_arr[i].data = '0;
+                cache_arr[i].valid = 0;
+                cache_arr[i].dirty = 0;
             end
-            // reset outputs
-            data <= '0;
-            dirty <= 1'b0;
-            valid <= 1'b0;
-            hit <= 1'b0;
-        end else if (valid && tag_arr[index] == tag) begin
-            hit <= 1'b1;
-            data <= data_arr[index];
-        end else begin
-            hit <= 1'b0;
+        end
+        else begin
+            if (fill_en) begin
+                if (fill_tag == cache_arr[fill_index].tag) begin
+                    cache_arr[fill_index].data <= fill_data;
+                    cache_arr[fill_index].dirty <= 1'b1;
+                    valid <= 1'b1;
+                    dirty <= 1'b0;
+                    hit <= 1'b0;
+                end
+            end
+            else if (cache_arr[index].valid & cache_arr[index].tag == tag) begin
+                data <= cache_arr[index].data;    
+                hit <= 1'b1;
+                valid <= 1'b1;
+                dirty <= cache_arr[index].dirty;
+            end
         end
     end
-          
 endmodule
